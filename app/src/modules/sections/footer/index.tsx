def5e081de18badba89e8fde5a4e3947137c799a
Lennert Van Geert <lennert.vangeert@gmail.com>
2025-05-11 22:52:19 +0200

    fix: update footer copyright year to current year dynamically

--- app/src/modules/sections/footer/index.tsx
+++ app/src/modules/sections/footer/index.tsx
@@ -2,11 +2,12 @@ import { Box, Center, Divider, Title } from "@mantine/core";
 
 const Footer = () => {
   return (
-    <Box  w="100%" py="2rem">
+    <Box w="100%" py="2rem">
       <Divider mb="2rem" />
       <Center px="1rem">
         <Title order={5}>
-          Made with ❤️ by Lennert - © 2025 All rights reserved.
+          Made with ❤️ by Lennert - © {new Date().getFullYear()} All rights
+          reserved.
         </Title>
       </Center>
     </Box>
